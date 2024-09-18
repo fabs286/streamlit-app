4cf0c806100e458d55944cc1ddb7e484088521f1
fabs286 <fabian.csoutsource@gmail.com>
2024-09-18 19:34:51 -0400

    Update streamlit.py
    Expand and contract axis automatically and permit 3 decimals in the input

--- streamlit.py
+++ streamlit.py
@@ -11,7 +11,7 @@ absorbancia_cal = np.array([0.1, 0.2, 0.4, 0.6, 0.8, 1.0])
 concentracion_cal = np.array([10, 20, 40, 60, 80, 100])
 
 # Crear un input numérico para ingresar el valor de absorbancia
-absorbancia_input = st.number_input('Ingresa el valor de absorbancia:', min_value=0.1, max_value=3.0, step=0.01, value=0.275)
+absorbancia_input = st.number_input('Ingresa el valor de absorbancia:', min_value=0.1, max_value=3.0, step=0.001, value=0.275, format="%.3f")
 
 # Crear la función de interpolación
 interp_func = interpolate.interp1d(absorbancia_cal, concentracion_cal, kind='linear', fill_value='extrapolate')
@@ -26,8 +26,8 @@ st.write(f"La concentración correspondiente a la absorbancia {absorbancia_input
 fig, ax = plt.subplots()
 
 # Limitar los ejes según los datos
-ax.set_xlim([0, max(concentracion_cal) * 1.1])
-ax.set_ylim([0, max(absorbancia_cal) * 1.1])
+ax.set_xlim([0, concentracion * 1.1])
+ax.set_ylim([0, absorbancia_input * 1.1])
 
 # Gráfica de la curva de calibración
 ax.plot(concentracion_cal, absorbancia_cal, label='Curva de Calibración (Calibrador)', color='blue')
