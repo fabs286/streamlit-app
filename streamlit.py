d36439e200778d1cad453f98d3a760b18722032a
fabs286 <fabian.csoutsource@gmail.com>
2024-09-19 01:12:45 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -60,16 +60,24 @@ concentraciones = [interp_func(absorbancia) for absorbancia in st.session_state.
 for i, (absorbancia, concentracion) in enumerate(zip(st.session_state.absorbancias_input, concentraciones), start=1):
     st.write(f"La concentración correspondiente a la absorbancia {absorbancia:.3f} (Resultado {i}) es: {concentracion:.2f} µIU/mL")
 
-# Verificar si es necesario extrapolar la curva
+# Verificar si es necesario extrapolar la curva, pero siempre ajustar según el máximo absorbancia
 max_absorbancia_input = max(absorbancias_actualizadas)
+
+# Obtener la pendiente de los últimos dos puntos de calibración
+slope = (concentracion_cal[-1] - concentracion_cal[-2]) / (absorbancia_cal[-1] - absorbancia_cal[-2])
+
+# Extender la curva de calibración más allá del punto máximo de absorbancia ingresado
 if max_absorbancia_input > absorbancia_cal[-1]:
-    # Extender la curva de calibración usando la pendiente de los últimos dos puntos
-    slope = (concentracion_cal[-1] - concentracion_cal[-2]) / (absorbancia_cal[-1] - absorbancia_cal[-2])
     extended_absorbancia = np.linspace(absorbancia_cal[-1], max_absorbancia_input * 1.2, num=50)
     extended_concentracion = concentracion_cal[-1] + slope * (extended_absorbancia - absorbancia_cal[-1])
-
     absorbancia_cal = np.concatenate((absorbancia_cal, extended_absorbancia[1:]))
     concentracion_cal = np.concatenate((concentracion_cal, extended_concentracion[1:]))
+elif max_absorbancia_input < absorbancia_cal[-1]:
+    # Recalcular a la escala de los resultados si el resultado es menor al último calibrador
+    absorbancia_cal_extended = np.linspace(0, max_absorbancia_input * 1.2, num=50)
+    concentracion_cal_extended = np.linspace(0, concentracion_cal[-1], num=50)
+    absorbancia_cal = absorbancia_cal_extended
+    concentracion_cal = concentracion_cal_extended
 
 # Generar la gráfica
 fig, ax = plt.subplots()
